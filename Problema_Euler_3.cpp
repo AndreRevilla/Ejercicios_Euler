1442da1a6faffac671508094d08ac0d57682cdc8
AndreRevilla <andre.revilla@ucsp.edu.pe>
2024-09-18 21:23:10 -0500

    Update Problema_Euler_3.cpp

--- Problema_Euler_3.cpp
+++ Problema_Euler_3.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int mayorFactor(long long num){
-    int mayor{1};
-
+bool esPrimo(int num) {
     int i=2;
+    if (num<=1){
+        return false;
+    }
     while(i < num){
         if (num % i == 0){
-            mayor = i;
+            return false;
         }
         i++;
+    }
+    return true;
+}
+
+int mayorFactorPrimo(long long n,int num){
+    int mayor{1};
+    int i=num;
+
+    while(i > 2){
+        if (n % i == 0){
+            if (esPrimo(i)){
+                mayor = i;
+                break;
+            }
+        }
+        i--;
     }
 
     return mayor;
 }
 
-
 int main(){
-    cout<<"El mayor factor primo de 600851475143 es: "<<mayorFactor(600851475143)<<endl;
+    long long n = 600851475143;
+    int num = sqrt(n);
+
+
+    cout<<"El mayor factor primo de 600851475143 es: "<<mayorFactorPrimo(n, num)<<endl;
 }
